7972e796b3831042cfa6f722e0a1c07251836f4a
Acc0LadE <142667881+Acc0LadE@users.noreply.github.com>
2025-03-17 19:08:13 +0530

    new commit task 4

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,5 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
+  cout << "I changed this code for the lab task 4 \n" << "PES2UG22CS398 \n";
   return 0;
 }
